fbe85e7ad8a8fe2641e8ce693636dc611207d269
rrluthi <rrluthi@gmail.com>
2023-11-30 23:38:00 -0800

    dedupe some code

--- day1/2.py
+++ day1/2.py
@@ -4,10 +4,15 @@ INT_STRINGS = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', '
 INT_STRINGS_REVERSED = ['eno', 'owt', 'eerht', 'ruof', 'evif', 'xis', 'neves', 'thgie', 'enin']
 
 
-def find_first_digit(line):
+def find_digit(line, reverse=False):
+    int_strings = INT_STRINGS
+    if reverse:
+        int_strings = INT_STRINGS_REVERSED
+
     str_index_found = float('inf')
     stored_i = float('inf')
-    for i, num in enumerate(INT_STRINGS):
+
+    for i, num in enumerate(int_strings):
         index = line.find(num)
         if index != -1 and index < str_index_found:
             stored_i = i
@@ -24,28 +29,6 @@ def find_first_digit(line):
     return first_digit
 
 
-def find_last_digit(line):
-    str_index_found = float('inf')
-    stored_i = float('inf')
-    reversed_line = line[::-1]
-    for i, num in enumerate(INT_STRINGS_REVERSED):
-        index = reversed_line.find(num)
-        if index != -1 and index < str_index_found:
-            str_index_found = index
-            stored_i = i
-
-    last_digit = str(stored_i + 1)
-
-    for i, char in enumerate(reversed_line):
-        if i > str_index_found:
-            break
-        if char in INTS:
-            last_digit = char
-            break
-
-    return last_digit
-
-
 def main():
     """
     open file and read line by line
@@ -54,7 +37,7 @@ def main():
     with open('input1.txt', 'r') as f:
         total = 0
         for line in f:
-            line_num = find_first_digit(line) + find_last_digit(line)
+            line_num = find_digit(line) + find_digit(line[::-1], True)
             total += int(line_num)
 
     print(total)
