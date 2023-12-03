b9e326dc7348476a9b38ce6ff424a58983f9dc8b
rrluthi <rrluthi@gmail.com>
2023-12-03 12:16:44 -0800

    progress on zig code, stuck casting []const u8 to u8

--- day2/1.zig
+++ day2/1.zig
@@ -51,19 +51,30 @@ fn parseGameLine(line: []const u8) !GameRecord {
     var game_split = split(u8, raw_values, "; ");
     while (game_split.next()) |game_value| {
         var colors_list = split(u8, game_value, ", ");
-        var cube_values = CubeValues{ .red = "0", .green = "0", .blue = "0" };
+        var cube_values = CubeValues{ .red = &[_]u8{0}, .green = &[_]u8{0}, .blue = &[_]u8{0} };
 
         while (colors_list.next()) |game| {
             const trimmed_color = std.mem.trim(u8, game, whitespace);
             var game_values = split(u8, trimmed_color, " ");
 
-            const number = (game_values.next() orelse return error.MissingValues)[0..];
+            
+            const number_slice = game_values.next();
+            const number: ?u8 = switch (number_slice) {
+                null => return error.MissingValues,
+                var slice => {
+                    if (slice.len == 0) {
+                        null
+                    } else {
+                        const first_element = slice[0];
+                        first_element
+                    }
+                },
+            };
             const color = (game_values.next() orelse return error.MissingKey)[0..];
             const field = stringToColor(color) orelse return error.MissingKey;
 
             setColor(&cube_values, field, number);
         }
-        std.debug.print("red: {s}, green: {s}, blue: {s} \n", .{ cube_values.red, cube_values.green, cube_values.blue });
         try values.append(cube_values);
     }
 
@@ -76,13 +87,27 @@ pub fn main() !void {
     var total: u8 = 0;
     var counter: u8 = 1;
     var splits = split(u8, file, "\n");
+    const limits = CubeValues{
+        .red = &[_]u8{12},
+        .green = &[_]u8{13},
+        .blue = &[_]u8{14},
+    };
+
     while (splits.next()) |line| {
-        _ = try parseGameLine(line);
-        //std.debug.print("Values: {s}\n", .{record.values.items});
-        total += 1;
+        const game_line = try parseGameLine(line);
+        var count_game_line = true;
+        for (game_line.values.items) |cubes| {
+            std.debug.print("Values: {s} {s} {s}\n", .{ cubes.red, cubes.green, cubes.blue });
+            if (limits.red > cubes.red or limits.green > cubes.green or limits.blue > cubes.blue) {
+                count_game_line = false;
+            }
+        }
+        if (count_game_line) {
+            total += counter;
+        }
+
         counter += 1;
-        std.debug.print("Counter: {d}, Total: {d} \n", .{ counter, total });
     }
 
-    std.debug.print("{d}", .{total});
+    std.debug.print("Total: {}", .{total});
 }
