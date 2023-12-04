62f32c6dbb987842e156bfc69e1be68231f158b9
rrluthi <rrluthi@gmail.com>
2023-12-03 22:20:50 -0800

    remove txt files

--- day2/1.zig
+++ day2/1.zig
@@ -6,9 +6,9 @@ const allocator = std.heap.page_allocator;
 const whitespace = " \t\n\r";
 
 const CubeValues = struct {
-    red: []const u8,
-    green: []const u8,
-    blue: []const u8,
+    red: u8,
+    green: u8,
+    blue: u8,
 };
 
 const Color = enum {
@@ -33,7 +33,7 @@ fn stringToColor(s: []const u8) ?Color {
     }
 }
 
-fn setColor(s: *CubeValues, color: Color, value: []const u8) void {
+fn setColor(s: *CubeValues, color: Color, value: u8) void {
     switch (color) {
         .red => s.red = value,
         .green => s.green = value,
@@ -44,32 +44,20 @@ fn setColor(s: *CubeValues, color: Color, value: []const u8) void {
 fn parseGameLine(line: []const u8) !GameRecord {
     var line_split = split(u8, line, ": ");
     _ = (line_split.next() orelse return error.MissingKey)[0..];
-    const raw_values = (line_split.next() orelse return error.MissingValues)[0..];
+    const raw_values = line_split.next().?;
 
     var values = std.ArrayList(CubeValues).init(allocator); // Dereference the pointer here
 
     var game_split = split(u8, raw_values, "; ");
     while (game_split.next()) |game_value| {
         var colors_list = split(u8, game_value, ", ");
-        var cube_values = CubeValues{ .red = &[_]u8{0}, .green = &[_]u8{0}, .blue = &[_]u8{0} };
+
+        var cube_values = CubeValues{ .red = 0, .green = 0, .blue = 0 };
 
         while (colors_list.next()) |game| {
             const trimmed_color = std.mem.trim(u8, game, whitespace);
             var game_values = split(u8, trimmed_color, " ");
-
-            
-            const number_slice = game_values.next();
-            const number: ?u8 = switch (number_slice) {
-                null => return error.MissingValues,
-                var slice => {
-                    if (slice.len == 0) {
-                        null
-                    } else {
-                        const first_element = slice[0];
-                        first_element
-                    }
-                },
-            };
+            const number: u8 = try std.fmt.parseInt(u8, game_values.next().?, 10);
             const color = (game_values.next() orelse return error.MissingKey)[0..];
             const field = stringToColor(color) orelse return error.MissingKey;
 
@@ -88,16 +76,16 @@ pub fn main() !void {
     var counter: u8 = 1;
     var splits = split(u8, file, "\n");
     const limits = CubeValues{
-        .red = &[_]u8{12},
-        .green = &[_]u8{13},
-        .blue = &[_]u8{14},
+        .red = 12,
+        .green = 13,
+        .blue = 14,
     };
 
     while (splits.next()) |line| {
         const game_line = try parseGameLine(line);
         var count_game_line = true;
         for (game_line.values.items) |cubes| {
-            std.debug.print("Values: {s} {s} {s}\n", .{ cubes.red, cubes.green, cubes.blue });
+            std.debug.print("Values: {d} {d} {d}\n", .{ cubes.red, cubes.green, cubes.blue });
             if (limits.red > cubes.red or limits.green > cubes.green or limits.blue > cubes.blue) {
                 count_game_line = false;
             }
